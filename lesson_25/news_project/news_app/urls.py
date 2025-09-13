00aa9bbae978c3b6831cac40651094c9b172154c
Uktam <uktamturgunov30@gmail.com>
2025-09-13 09:32:31 +0500

    Add lesson 25: Django News Project

---
+++ lesson_25/news_project/news_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import HomePageView, SinglePageView, NewsListView, AboutPageView, ContactPageView, custom_404_view
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('news/', NewsListView.as_view(), name='news_list'),
+    path('news/<slug:slug>/', SinglePageView.as_view(), name='news_detail'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('404/', custom_404_view, name='404'),
+]
