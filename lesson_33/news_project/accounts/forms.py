790fe92a84830649a9f09da692fdcca1ccb7cf77
Uktam <uktamturgunov30@gmail.com>
2025-09-22 10:30:57 +0500

    Implement password change functionality with templates

--- lesson_33/news_project/accounts/forms.py
+++ lesson_33/news_project/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
 
 class LoginForm(forms.Form):
   username = forms.CharField(
@@ -9,3 +10,8 @@ class LoginForm(forms.Form):
     label="Password",
     widget=forms.PasswordInput(attrs={'class': 'form-control'}) 
   )
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
