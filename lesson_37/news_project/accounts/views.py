7ecb71ca383beb13c1e47da7b49397ca7b868d5b
Uktam <uktamturgunov30@gmail.com>
2025-09-25 11:08:53 +0500

    Refactor signup view to CBV and update related URLs

--- lesson_37/news_project/accounts/views.py
+++ lesson_37/news_project/accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views import View
 
 # Create your views here.
 def user_login(request):
@@ -37,12 +39,37 @@ def dashboard_view(request):
 
   return render(request, 'pages/user_profile.html', context)
 
-def signup_view(request):
-    if request.method == "POST":
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()  # user saqlanadi
-            return render(request, "accounts/register_done.html")  
-    else:
-        form = UserRegistrationForm()
-    return render(request, "accounts/register.html", {"form": form})
+# variant 1 signup view uchun function bilan
+# def signup_view(request):
+#     if request.method == "POST":
+#         form = UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()  # user saqlanadi
+#             return render(request, "accounts/register_done.html")  
+#     else:
+#         form = UserRegistrationForm()
+#     return render(request, "accounts/register.html", {"form": form})
+
+# variant 2 signup view CreateView bilan
+class SignUpView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('accounts:register_done')
+
+    def form_valid(self, form):
+      # form.save() parolni ham hashlaydi, chunki biz save() metodini override qilganmiz
+      form.save()
+      return super().form_valid(form) 
+    
+# variant 3 signup view uchun View bilan
+# class SignupView(View):
+#     def get(self, request):
+#         form = UserRegistrationForm()
+#         return render(request, "accounts/register.html", {"form": form})
+    
+#     def post(self, request):
+#         form = UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()  # user saqlanadi
+#             return render(request, "accounts/register_done.html")  
+#         return render(request, "accounts/register.html", {"form": form})
