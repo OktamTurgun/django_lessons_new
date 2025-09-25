53deb9397b0200a98e73ec0231418732887e3736
Uktam <uktamturgunov30@gmail.com>
2025-09-25 09:38:36 +0500

    Add first_name and last_name fields to UserRegistrationForm

--- lesson_36/news_project/accounts/forms.py
+++ lesson_36/news_project/accounts/forms.py
@@ -29,10 +29,12 @@ class UserRegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
     def clean(self):
